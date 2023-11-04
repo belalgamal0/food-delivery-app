7dac84e92dfc7524a3d3ad57426ef68202e7d54f
belalgamal0 <belal.gamal09@gmail.com>
2023-11-04 02:15:47 +0200

    core folder
    create core folder for shared components, features

---
+++ lib/core/theme/app_color_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColorTheme {
+  static const Color appPimaryColor = Color(0xfff69d7b);
+}
---
+++ lib/core/theme/app_text_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTextTheme{
+  static const TextStyle black14Bold = TextStyle(fontWeight: FontWeight.bold);
+  static const TextStyle black14Regular = TextStyle(color: Colors.black);
+  static const TextStyle black17Bold = TextStyle(fontWeight: FontWeight.bold,fontSize: 17);
+  
+}
